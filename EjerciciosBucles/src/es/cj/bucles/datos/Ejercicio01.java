29e3157496251a5d6be30d5886bfce01b2e55212
Alumno <Alumno@AULA29-4>
2018-10-15 11:15:02 +0200

    15/10/18

---
+++ EjerciciosBucles/src/es/cj/bucles/datos/Ejercicio01.java
@@ -0,0 +1,42 @@
+package es.cj.bucles.datos;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+
+		// Pedir 10 números. Mostrar la media de los positivos, media de negativos y cantidad de ceros.
+		
+		double sumaPositivos = 0, sumaNegativos = 0;
+		int contadorPositivos = 0, contadorNegativos = 0, contadorCeros = 0;
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Nº: ");
+			double numero = sc.nextDouble();
+			
+			if (numero > 0) {
+				// sumaPositivos = sumaPositivos + numero;
+				sumaPositivos += numero;
+				contadorPositivos++;
+			} else if (numero < 0) {
+				sumaNegativos += numero;
+				contadorNegativos++;
+			} else {
+				contadorCeros++;
+			}
+			
+			
+		}
+		
+		System.out.println("Media positivos: " + (sumaPositivos/contadorPositivos));
+		System.out.println("Media negativos: " + (sumaNegativos/contadorNegativos));
+		System.out.println("Contador de ceros: " + contadorCeros);
+		
+		
+		sc.close();
+	}
+
+}
---
+++ EjerciciosBucles/src/es/cj/bucles/datos/Tarea01.java
@@ -0,0 +1,24 @@
+package es.cj.bucles.datos;
+
+import java.util.Scanner;
+
+public class Tarea01 {
+
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		// Programa que muestre y calcule el producto de los n primeros números impares.
+		// Nota: si metes 10, multiplica los 10 primeros números impares.
+		
+		int numero = sc.nextInt();
+		
+		for 
+		
+		
+		
+		sc.close();
+
+	}
+
+}
