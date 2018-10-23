16fcb5cdd1b4fafdbf8e69092c33cd518c1ff729
Alumno <Alumno@AULA29-4>
2018-10-23 14:41:54 +0200

    d

--- EjerciciosTablas/src/es/cj/ejerciciostablas/datos/Ejercicio02.java
+++ EjerciciosTablas/src/es/cj/ejerciciostablas/datos/Ejercicio02.java
@@ -1,7 +1,14 @@
 package es.cj.ejerciciostablas.datos;
 
+import java.util.Scanner;
+
 public class Ejercicio02 {
 
+	private static Scanner sc = new Scanner(System.in);
+	
+	// Leer n números ordenados crecientemente. Pedir al usuario un número y buscarlo en la tabla. 
+	// Se debe mostrar la posición en la que se encuentra. Si no está, indicarlo con un mensaje.
+	
 	public static void main(String[] args) {
 		
 
