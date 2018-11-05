b389256e3b50285e99d827bd36a8df0d18bb6e30
Alumno <Alumno@AULA29-4>
2018-11-05 11:07:56 +0100

    05/11/2018

---
+++ EjerciciosSemanales/src/es/cj/ejerciciossemanales/Ejercicio05.java
@@ -0,0 +1,34 @@
+package es.cj.ejerciciossemanales;
+
+import java.util.Random;
+
+public class Ejercicio05 {
+
+	// Rellena una tabla de 10 números (valores aleatorios del 1 al 6). Reliza una
+	// función que devuelva el número que más se repite.
+
+	private static int[] tabla;
+
+	public static void main(String[] args) {
+
+		iniTabla();
+		ordenarTabla();
+
+	}
+
+	private static void iniTabla() {
+		
+		for (int i = 0; i < tabla.length; i++) {
+			for (int j = 0; j < tabla[i].length; j++) {
+				tabla[i] = new Random().nextInt(10);
+		
+		
+	}
+
+	private static void ordenarTabla() {
+}
+
+	for (i)
+		if (repetidos[i] > mayor)
+			mayor = repetidos [i];
+	}
